1a0793944cd98ab66e738e00d55a0013d036284c
Amanda Kado <105023810+AmandaKado@users.noreply.github.com>
2023-10-14 04:30:41 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-// Função para copiar o link gerado para a área de transferência
+
 function copiarLinkParaAreaDeTransferencia(link) {
-    // Cria um elemento de input oculto
+
     const input = document.createElement("input");
     input.style.position = "fixed";
     input.style.opacity = 0;
     input.value = link;
 
-    // Adiciona o elemento à página
+    
     document.body.appendChild(input);
 
-    // Seleciona o conteúdo do input
+    
     input.select();
     input.setSelectionRange(0, 99999); // Para dispositivos móveis
 
-    // Copia o texto para a área de transferência
     document.execCommand("copy");
 
-    // Remove o elemento de input
+    
     document.body.removeChild(input);
 }
 
-// Função para gerar o link do WhatsApp com mensagem personalizada e copiá-lo
 function gerarLinkWhatsApp() {
     const numero = document.getElementById("numero").value;
     const mensagem = document.getElementById("mensagem").value;
@@ -29,7 +27,7 @@ function gerarLinkWhatsApp() {
     if (numero) {
         // Formate o número removendo caracteres não numéricos
         const numeroFormatado = numero.replace(/\D/g, "");
-        // Monte o link com a mensagem, incluindo nome e mensagem personalizada
+       
         const link = `https://wa.me/${numeroFormatado}?text=${encodeURIComponent(mensagem)}`;
 
         // Copie o link para a área de transferência
@@ -41,6 +39,5 @@ function gerarLinkWhatsApp() {
     }
 }
 
-// Event listener para o botão "Gerar Link"
 const gerarLinkButton = document.getElementById("gerarLink");
 gerarLinkButton.addEventListener("click", gerarLinkWhatsApp);
